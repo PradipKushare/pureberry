a44a0ca6283bfb76932e04917ae173069fa15172
Michael Pereira <Michaelp17@rocketmail.com>
2020-01-21 11:23:15 +0530

    changes in components

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 class NotFound extends Component {
     render() {
-        return (
-            <h1>Not Found Component</h1>
-         )
+        return ( <
+            img className = "no-img-found"
+            src = "https://cdn.dribbble.com/users/2495203/screenshots/5697772/404_page_not_found.jpg" / >
+        )
     }
 }
 
--- src/components/sub_parts/Header.js
+++ src/components/sub_parts/Header.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';  
+import React, { Component } from 'react';
 
 class Header extends Component {
 
-  constructor(props){
-      super(props);
-  }  
+    constructor(props) {
+        super(props);
+    }
 
 
-  render(){
-    return (
-           <div>
-             <center> Here is Header component</center>
-           </div>
-    )
-  }
+    render() {
+        return ( <
+            div >
+            <
+            center > Here is Header component < /center>  < /
+            div >
+        )
+    }
 
 }
 
